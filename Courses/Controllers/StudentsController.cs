8025dd120f10a264bfbf14b1f46e148caf38cc4e
hp <hp@DESKTOP-S4P3ABE>
2021-04-30 04:20:31 -0700

    Listing Of Students

---
+++ Courses/Controllers/StudentsController.cs
@@ -0,0 +1,30 @@
+﻿using Courses.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Courses.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private AppDbContext db;
+
+        public StudentsController()
+        {
+            db = new AppDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            var students = db.Students.ToList();
+            return View(students);
+        }
+    }
+}
