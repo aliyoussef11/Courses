f83a220d72ba5bf36dd83ea9e4e7c0e1cc377470
hp <hp@DESKTOP-S4P3ABE>
2021-04-30 22:36:19 -0700

    Register students in courses (course assignment page) using Form with Validations.

---
+++ Courses/Controllers/EnrollmentsController.cs
@@ -0,0 +1,66 @@
+﻿using Courses.Models;
+using Courses.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Courses.Controllers
+{
+    public class EnrollmentsController : Controller
+    {
+        private AppDbContext db;
+
+        public EnrollmentsController()
+        {
+            db = new AppDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+
+        // GET: Enrollments
+        public ActionResult Register()
+        {
+            var admissionTermCodes = db.AdmissionTermCodes.ToList();
+            var majors = db.Majors.ToList();
+            var courses = db.Courses.ToList();
+            var students = db.Students.ToList();
+            var viewModel = new NewEnrollmentViewModel
+            {
+                admissionTermCodes = admissionTermCodes,
+                majors = majors,
+                courses = courses,
+                students = students
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult RegisterStudentInCourse(Enrollment enrollment)
+        {
+            if (!ModelState.IsValid)
+            {
+                var admissionTermCodes = db.AdmissionTermCodes.ToList();
+                var majors = db.Majors.ToList();
+                var courses = db.Courses.ToList();
+                var students = db.Students.ToList();
+                var viewModel = new NewEnrollmentViewModel
+                {
+                    admissionTermCodes = admissionTermCodes,
+                    majors = majors,
+                    courses = courses,
+                    students = students,
+                    Enrollment = enrollment
+                };
+                return View("Register", viewModel);
+            }
+            db.Enrollments.Add(enrollment);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Courses/ViewModels/NewEnrollmentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Courses.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Courses.ViewModels
+{
+    public class NewEnrollmentViewModel
+    {
+        public IEnumerable<AdmissionTermCode> admissionTermCodes { get; set; }
+
+        public IEnumerable<Student> students { get; set; }
+
+        public IEnumerable<Course> courses { get; set; }
+
+        public IEnumerable<Major> majors { get; set; }
+
+        public Enrollment Enrollment { get; set; }
+    }
+}
